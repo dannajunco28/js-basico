3d140d84eaedd71081d2b073b558c1689ed12bf2
dannajunco28 <dgjunco@misena.edu.co>
2022-05-16 15:18:52 -0500

    variables

--- assests/js/cadenas.js
+++ assests/js/cadenas.js
@@ -1,6 +1,11 @@
 const parrafo = document.getElementById("parrafo");
-
 const boton = document.querySelector("button");
+
+const caracter1 = document.getElementById("caracter1");
+const caracter2 = document.getElementById("caracter2");
+const caracter3 = document.getElementById("caracter3");
+const caracter4 = document.getElementById("caracter4");
+const caracter5 = document.getElementById("caracter5");
 
 let cita1 = "Caminando en línea recta ";
 let cita2 = "no puede uno llegar muy lejos";
@@ -56,19 +61,53 @@ parrafo.textContent = citaCompleta;
 
 //Ejercicios
 
-//MAN675847583748sjt567654;Manchester Piccadilly
-//GNF576746577363dhg23465s;Greenfield
-//LIV63748827374trye738837;Liverpool Lime Street
-//SYB4ferydh5tisztTs56478f;Stalybridge
-//HUD7tyu64776490uHjGghut4;Huddersfiel
+//MAN675847583748sjt567654; Manchester Piccadilly
+//GNF576746577363dhg23465s; Greenfield
+//LIV63748827374trye738837; Liverpool Lime Street
+//SYB4ferydh5tisztTs56478f; Stalybridge
+//HUD7tyu64776490uHjGghut4; Huddersfiel
 
-//MAN; Manchester Piccadilly
+//MAN: Manchester Piccadilly
 
-let variable ="MAN675847583748sjt567654;Manchester Piccadilly"
-console.log(variable)
+let variable1 ="MAN675847583748sjt567654;Manchester Piccadilly"
+console.log(variable1);
 
+let Man1 = "MAN675847583748sjt567654";
+let Man2 = "; Manchester Piccadilly";
+let ManCompleto = Man1.slice(0,3);
+Man2 = Man2.replace(";",":");
+caracter1.textContent = ManCompleto + Man2;
 
+//codigo2 GNF576746577363dhg23465s; Greenfield
 
+let Gnf1 = "GNF576746577363dhg23465s";
+let Gnf2 = "; Greenfield";
+let GnfCompleto = Gnf1.slice(0,3);
+Gnf2 = Gnf2.replace(";",":");
+caracter2.textContent = GnfCompleto + Gnf2;
 
+//codigo 3 LIV63748827374trye738837; Liverpool Lime Street
+
+let liv1 = "LIV63748827374trye738837";
+let liv2 = "; Liverpool Lime Street";
+let livCompleto = liv1.slice (0,3);
+liv2 = liv2.replace(";",":");
+caracter3.textContent = livCompleto + liv2;
+
+//codigo 4 SYB4ferydh5tisztTs56478f; Stalybridge
+
+let syb1= "SYB4ferydh5tisztTs56478f";
+let syb2 = "; Stalybridge ";
+let sybCompleto = syb1.slice (0,3);
+syb2 = syb2.replace(";",":");
+caracter4.textContent = sybCompleto + syb2;
+
+//codigo 5 HUD7tyu64776490uHjGghut4; Huddersfiel
+
+let hud1 = "HUD7tyu64776490uHjGghut4"
+let hud2 = "; Stalybridge ";
+let hudCompleto = hud1.slice (0,3);
+hud2 = hud2.replace (";",":");
+caracter5.textContent = hudCompleto + hud2;
 
 
